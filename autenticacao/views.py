212390930c3b6e6a4a3938fa3d3692491eb868f7
Ana Mendes <anajessica.mdo@gmail.com>
2022-07-05 11:35:27 -0300

    Adicionando utils.py ao projeto

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from .utils import password_is_valid
 
 def cadastro(request):
     if request.method == "GET":
         # resposta http que renderiza uma página html
         return render(request, 'cadastro.html')
     elif request.method == "POST":
-        usuario = request.POST.get('usuario')
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
         email = request.POST.get('email')
-        return HttpResponse(f'{usuario} e {email}')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        return HttpResponse(confirmar_senha)
 
 def logar(request):
     return HttpResponse("Você está na página de Login!")
