07e25e97daedc580966db93577b25733a6eda1ab
Ana Mendes <anajessica.mdo@gmail.com>
2022-07-05 22:40:29 -0300

    adicionando página de login

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 from .utils import password_is_valid
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == "GET":
@@ -12,7 +17,34 @@ def cadastro(request):
         email = request.POST.get('email')
         confirmar_senha = request.POST.get('confirmar_senha')
 
-        return HttpResponse(confirmar_senha)
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro')
+
+        try:
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=senha,
+                                            is_active=False)
+            user.save()
+            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso')
+            return redirect('/auth/logar')
+
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno no sistema')
+            return redirect('/auth/cadastro')
 
 def logar(request):
-    return HttpResponse("Você está na página de Login!")
+    if request.method == "GET":
+        return render(request, 'logar.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+
+        usuario = auth.authenticate(username=username, password=senha)
+
+        if not usuario:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect('/auth/logar')
+        else:
+            auth.login(request, usuario)
+            return redirect('/')
--- nutri_lab/settings.py
+++ nutri_lab/settings.py
@@ -128,3 +128,17 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+# as 5 mensagens de erro que o django traz
+# os alerts são referenciadas no bootstrap.
+
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
